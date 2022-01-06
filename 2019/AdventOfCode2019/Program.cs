9e59c2b57827b5fa1dc580621595d2dd3b549846
Maxim Baranov <atmbaranov@gmail.com>
2022-01-06 20:00:09 +0300

    Advent Of Code 2019 - Day 22

--- 2019/AdventOfCode2019/Program.cs
+++ 2019/AdventOfCode2019/Program.cs
@@ -10,8 +10,8 @@ namespace AdventOfCode2019
         {
             Input.HttpSession = Environment.GetEnvironmentVariable("ADVENT_OF_CODE_SESSION");
 
-            var problem = new Day21.Part2();
-            await problem.Run(Day21.TestInput);
+            var problem = new Day22.Part2();
+            await problem.Run(Day22.TestInput);
 
             Console.ReadLine();
         }
