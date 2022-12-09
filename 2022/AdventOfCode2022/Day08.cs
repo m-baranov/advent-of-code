97e9f0a86556a64853f8eb053d1fd2008d583222
Maxim Baranov <atmbaranov@gmail.com>
2022-12-10 01:54:32 +0300

    Advent Of Code 2022: Days 8-9

---
+++ 2022/AdventOfCode2022/Day08.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2022
+{
+    static class Day08
+    {
+        public static class Inputs
+        {
+            public static readonly IInput Sample =
+                Input.Literal(
+                    "30373",
+                    "25512",
+                    "65332",
+                    "33549",
+                    "35390"
+                );
+
+            public static readonly IInput Test =
+                Input.Http("https://adventofcode.com/2022/day/8/input");
+        }
+
+        public class Part1 : IProblem
+        {
+            public void Run(TextReader input)
+            {
+                var grid = new Grid(input.Lines().ToList());
+
+                var count = Position.AllInGrid(grid.Rows, grid.Cols)
+                    .Where(grid.IsVisibleAt)
+                    .Count();
+
+                Console.WriteLine(count);
+            }
+        }
+
+        public class Part2 : IProblem
+        {
+            public void Run(TextReader input)
+            {
+                var grid = new Grid(input.Lines().ToList());
+
+                var max = Position.AllInGrid(grid.Rows, grid.Cols)
+                    .Select(grid.ScenicScoreOf)
+                    .Max();
+
+                Console.WriteLine(max);
+            }
+        }
+
+        public record Position(int Row, int Col)
+        {
+            public static IEnumerable<Position> LeftOf(Position p)
+            {
+                var col = p.Col - 1;
+                while (col >= 0)
+                {
+                    yield return p with { Col = col };
+                    col--;
+                }
+            }
+
+            public static IEnumerable<Position> RightOf(Position p, int cols)
+            {
+                var col = p.Col + 1;
+                while (col < cols)
+                {
+                    yield return p with { Col = col };
+                    col++;
+                }
+            }
+
+            public static IEnumerable<Position> TopOf(Position p)
+            {
+                var row = p.Row - 1;
+                while (row >= 0)
+                {
+                    yield return p with { Row = row };
+                    row--;
+                }
+            }
+
+            public static IEnumerable<Position> BottomOf(Position p, int rows)
+            {
+                var row = p.Row + 1;
+                while (row < rows)
+                {
+                    yield return p with { Row = row };
+                    row++;
+                }
+            }
+
+            public static IEnumerable<Position> AllInGrid(int rows, int cols)
+            {
+                for (var row = 0; row < rows; row++)
+                {
+                    for (var col = 0; col < cols; col++)
+                    {
+                        yield return new Position(row, col);
+                    }
+                }
+            }
+        }
+
+        private class Grid
+        {
+            private readonly IReadOnlyList<string> cells;
+
+            public Grid(IReadOnlyList<string> cells)
+            {
+                this.cells = cells;
+            }
+
+            public int Rows => this.cells.Count;
+            public int Cols => this.cells[0].Length;
+
+            public int HeightAt(Position p) => this.cells[p.Row][p.Col] - '0';
+
+            public bool IsVisibleAt(Position p)
+            {
+                var height = HeightAt(p);
+
+                var directions = new[]
+                {
+                    Position.LeftOf(p),
+                    Position.RightOf(p, this.Cols),
+                    Position.TopOf(p),
+                    Position.BottomOf(p, this.Rows)
+                };
+
+                return directions.Any(d => d.Select(HeightAt).All(h => h < height));
+            }
+
+            public int ScenicScoreOf(Position p)
+            {
+                static int Multiply(IEnumerable<int> nums) => nums.Aggregate(1, (acc, n) => acc * n);
+
+                int CountVisible(int height, IEnumerable<Position> positions)
+                {
+                    var count = 0;
+                    foreach (var p in positions)
+                    {
+                        var h = HeightAt(p);
+                        count++;
+                        if (h >= height)
+                        {
+                            break;
+                        }
+                    }
+                    return count;
+                }
+
+                var height = HeightAt(p);
+
+                var directions = new[]
+                {
+                    Position.LeftOf(p),
+                    Position.RightOf(p, this.Cols),
+                    Position.TopOf(p),
+                    Position.BottomOf(p, this.Rows)
+                };
+
+                return Multiply(directions.Select(d => CountVisible(height, d)));
+            }
+        }
+    }
+}
---
+++ 2022/AdventOfCode2022/Day09.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2022
+{
+    static class Day09
+    {
+        public static class Inputs
+        {
+            public static readonly IInput Sample1 =
+                Input.Literal(
+                    "R 4",
+                    "U 4",
+                    "L 3",
+                    "D 1",
+                    "R 4",
+                    "D 1",
+                    "L 5",
+                    "R 2"
+                );
+
+            public static readonly IInput Sample2 =
+                Input.Literal(
+                    "R 5",
+                    "U 8",
+                    "L 8",
+                    "D 3",
+                    "R 17",
+                    "D 10",
+                    "L 25",
+                    "U 20"
+                );
+
+            public static readonly IInput Test =
+                Input.Http("https://adventofcode.com/2022/day/9/input");
+        }
+
+        public class Part1 : IProblem
+        {
+            public void Run(TextReader input)
+            {
+                var moves = Move.Parse(input.Lines());
+
+                var rope = Rope.Initial(length: 2);
+
+                var tailPoints = new HashSet<Point>() { rope.Tail };
+
+                foreach (var move in moves)
+                {
+                    for (var i = 0; i < move.Steps; i++)
+                    {
+                        rope = rope.Move(move.Direction);
+                        tailPoints.Add(rope.Tail);
+                    }
+                }
+
+                Console.WriteLine(tailPoints.Count);
+            }
+        }
+
+        public class Part2 : IProblem
+        {
+            public void Run(TextReader input)
+            {
+                var moves = Move.Parse(input.Lines());
+
+                var rope = Rope.Initial(length: 10);
+
+                var tailPoints = new HashSet<Point>() { rope.Tail };
+
+                foreach (var move in moves)
+                {
+                    for (var i = 0; i < move.Steps; i++)
+                    {
+                        rope = rope.Move(move.Direction);
+                        tailPoints.Add(rope.Tail);
+                    }
+                }
+
+                Console.WriteLine(tailPoints.Count);
+            }
+        }
+
+        private record Point(int X, int Y)
+        {
+            public static readonly Point Origin = new(0, 0);
+
+            public Point Move(Direction direction) =>
+                direction switch
+                {
+                    Direction.L => this with { X = X - 1 },
+                    Direction.R => this with { X = X + 1 },
+                    Direction.U => this with { Y = Y - 1 },
+                    Direction.D or _ => this with { Y = Y + 1 }
+                };
+        }
+
+        private record Rope(IReadOnlyList<Point> Points)
+        {
+            public static Rope Initial(int length)
+            {
+                var points = Enumerable.Range(0, length).Select(_ => Point.Origin).ToList();
+                return new Rope(points);
+            }
+
+            public Point Head => this.Points[0];
+            public Point Tail => this.Points[this.Points.Count - 1];
+               
+            public Rope Move(Direction direction)
+            {
+                static int Cap(int value) =>
+                    value switch
+                    {
+                        0 => 0,
+                        < 0 => -1,
+                        > 0 => 1
+                    };
+
+                static Point NextTail(Point head, Point tail)
+                {
+                    var dx = head.X - tail.X;
+                    var dy = head.Y - tail.Y;
+
+                    if (Math.Abs(dx) < 2 && Math.Abs(dy) < 2)
+                    {
+                        return tail;
+                    }
+                    if (dx == 0)
+                    {
+                        return tail with { Y = tail.Y + Cap(dy) };
+                    }
+                    if (dy == 0)
+                    {
+                        return tail with { X = tail.X + Cap(dx) };
+                    }
+
+                    return new Point(tail.X + Cap(dx), tail.Y + Cap(dy));
+                }
+
+                var nextPoints = new List<Point>() { Head.Move(direction) };
+
+                for (var i = 1; i < this.Points.Count; i++)
+                {
+                    nextPoints.Add(NextTail(nextPoints[i - 1], this.Points[i]));
+                }
+
+                return new Rope(nextPoints);
+            }
+        }
+
+        private enum Direction { L, R, U, D }
+
+        private record Move(Direction Direction, int Steps)
+        {
+            public static IEnumerable<Move> Parse(IEnumerable<string> lines) =>
+                lines.Select(Parse);
+
+            public static Move Parse(string line)
+            {
+                var parts = line.Split(' ');
+
+                var dir = ParseDirection(parts[0]);
+                var steps = int.Parse(parts[1]);
+
+                return new Move(dir, steps);
+            }
+
+            private static Direction ParseDirection(string dir) =>
+                dir switch
+                {
+                    "L" => Direction.L,
+                    "R" => Direction.R,
+                    "U" => Direction.U,
+                    "D" or _ => Direction.D
+                };
+        }
+    }
+}
